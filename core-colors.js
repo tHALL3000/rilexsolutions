e2633501590d41315606e74b5e76fc3c155827b3
tHALL3000 <80265634+tHALL3000@users.noreply.github.com>
2023-07-06 16:12:30 -0600

    Create core-colors.js

---
+++ core-colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  bodyBackground: '#eae8e2',
+  headingColor: '#1bcd63',
+  linkColor: '#41a9b7',
+  linkHoverColor: '#515159',
+  buttonBackground: '#1bcd63',
+  buttonHoverBackground: '#515159',
+};
